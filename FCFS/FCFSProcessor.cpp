719e151b803c085f089c6c157aacd313b5d9ed23
Jure Jumalon <jjumalon13@stetson.edu>
2013-10-21 18:02:43 -0400

    FCFSProcessor.cpp: Fixed again because Jacob is bad.

--- FCFS/FCFSProcessor.cpp
+++ FCFS/FCFSProcessor.cpp
@@ -12,13 +12,12 @@ int main()
 	int priorities[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 	Process *pproc = new Process[10];
-	pproc[0] -> runtime = 10;
 	//Process procs[10];
 	for(int i = 0; i < 10; i++)
 	{
-		procs[i]->runtime = runtimes[i];
-		procs[i]->deadlines = deadlines[i];
-		procs[i]->priorities = priorities[i];
+		pproc[i].runtime = runtimes[i];
+		pproc[i].deadline = deadlines[i];
+		pproc[i].priority = priorities[i];
 	}
 	
 	return 0;
