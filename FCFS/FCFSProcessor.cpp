0b31efd3bbe640cfc1e6d5e0d0e704847044b0a2
jnighlight <jlites@stetson.edu>
2013-10-21 17:06:31 -0400

    Got the file working to read the txt file and print everything out on different lines It's not that big a deal yet, but will help when we read in process information. GLHF

---
+++ FCFS/FCFSProcessor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream myFile;
+	string line;
+	myFile.open("FCFSProcesses.txt");
+	if(myFile.is_open())
+	{
+		while ( getline (myFile,line) )
+			{cout << line << endl;}
+		myFile.close();
+	}
+	else
+	{
+		cout << "Way to suck bro";
+	}
+	
+	return 0;
+}
