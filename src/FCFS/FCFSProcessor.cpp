2dcc42d884fec93a9c18275a339e5f93d23104d9
jnighlight <jlites@stetson.edu>
2013-10-22 00:36:56 -0400

    All good! FCFS is working, we just need to mine data from it. ON TO OTHER THINGS

--- src/FCFS/FCFSProcessor.cpp
+++ src/FCFS/FCFSProcessor.cpp
@@ -8,15 +8,13 @@ using namespace std;
 
 bool processSortByArriveTime(Process procA, Process procB)
 {
-	return procA.arriveTime <= procB.arriveTime;
+	return procA.arrive_time <= procB.arrive_time;
 }
 
 int main()
 {
-	int runtimes[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int deadlines[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int priorities[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int arrivalTimes[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int runtimes[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
+	int arrivalTimes[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
 	int processesLeft = 10;
 	
 	int clockerSpaniel = 0;
@@ -28,33 +26,50 @@ int main()
 	for(int i = 0; i < 10; i++)
 	{
 		procs[i].runtime = runtimes[i];
-		procs[i].deadline = deadlines[i];
-		procs[i].priority = priorities[i];
 		procs[i].arrive_time = arrivalTimes[i];
 	}
 	
 	std::sort(procs, procs + processesLeft, &processSortByArriveTime);
 	
-	while(procs[0] != NULL)
+	int doneRun = 0;
+	while(procs[0].runtime != -1)
 	{
-		if(procs[i].runtime > 0)
+		clockerSpaniel++;
+		if(procs[0].runtime - doneRun > 0)
 		{
-			procs[0].runtime--;
-			cout << "The priority is: " << procs[0].priority << endl;
+			doneRun++;
+			//cout << "The arrival time is: " << procs[0].arrive_time << endl;
 		}
 		else
 		{
-			procs[i].wait_time = clockerSpaniel - procs[i].arriveTime - procs[i].runtime;
-			procs[i].processing_time = clockerSpaniel - procs[i].arriveTime;
+			cout << "Switching Processes " << endl;
+			cout << "clock: " << clockerSpaniel << endl;
+			cout << "arrive_time: " << procs[0].arrive_time << endl;
+			cout << "runtime: " << procs[0].runtime << endl << endl;
+			procs[0].wait_time = (clockerSpaniel - procs[0].arrive_time) - procs[0].runtime;
+			procs[0].processing_time = clockerSpaniel - procs[0].arrive_time;
+			procs[0].runtime = -1;
+			doneRun = 0;
 			
 			processesLeft--;
-			for(int j = 0; j < processesLeft; j++)
+			Process holder = procs[0];
+			for(int j = 0; j < 10; j++)
 			{
 				procs[j] = procs[j+1];
 			}
-			procs[processesLeft] = NULL;
+			procs[9] = holder;
 		}
-		clockerSpaniel++;
+		//clockerSpaniel++;
+	}
+	
+	cout << endl;
+	
+	for(int i = 0; i < 10; i++)
+	{
+		
+		cout << endl << "Process Number: " << i+1 << endl;
+		cout << procs[i].wait_time  << endl;
+		cout << procs[i].processing_time  << endl;
 	}
 	
 	return 0;
