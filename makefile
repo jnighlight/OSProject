e745c3a903a4d92c69463efbee016e0a9cb2d81d
Jure Jumalon <jjumalon13@stetson.edu>
2013-10-22 00:55:37 -0400

    Updated Round Robin.

--- makefile
+++ makefile
@@ -10,4 +10,10 @@ fcfs : $(ODIR)FCFSProcessor.o
 	$(CC) -o $(BDIR)fcfs $(ODIR)FCFSProcessor.o $(CFLAGS) 
 	
 $(ODIR)FCFSProcessor.o : $(SDIR)FCFS/FCFSProcessor.cpp ./include/Process.h 
-	(CC) -c -o $(ODIR)FCFSProcessor.o $(SDIR)FCFS/FCFSProcessor.cpp $(CFLAGS)
+	$(CC) -c -o $(ODIR)FCFSProcessor.o $(SDIR)FCFS/FCFSProcessor.cpp $(CFLAGS)
+
+round_robin : $(ODIR)round_robin.o
+	$(CC) -o $(BDIR)round_robin $(ODIR)round_robin.o $(CFLAGS) 
+	
+$(ODIR)round_robin.o : $(SDIR)Round\ Robin/main.cpp ./include/Process.h 
+	$(CC) -c -o $(ODIR)round_robin.o $(SDIR)Round\ Robin/main.cpp $(CFLAGS)
--- src/Round Robin/main.cpp
+++ src/Round Robin/main.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <iostream>
+#include <string>
 #include <Process.h>
 
 #define TIME_QUANTUM 100
@@ -7,37 +9,48 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-	// Set the number of processes
-	int processes = 10;
-
 	// Initialize process values
-	int runtimes[processes]      = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
-	int arrival_times[processes] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+	int processes = 10;
+	int runtimes[10]      = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+	int arrival_times[10] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 
-	Process proccess[processes];
+	Process process[processes];
 	for(int i = 0; i < processes; i++)
 	{
-		proccess[i].runtime         = runtimes[i];
-		proccess[i].arrive_time     = arrival_times[i];
-		proccess[i].processing_time = 0;
+		process[i].runtime         = runtimes[i];
+		process[i].arrive_time     = arrival_times[i];
+		process[i].processing_time = 0;
 	}
 
 	// Begin Round Robin
 	int clock = 0;
 	int runtime = 0;
-	int current_process = 0;
+	int cproc = 0;
 
-	while(proccess > 0)
+	while(processes > 0)
 	{
-		while(runtime < TIME_QUANTUM)
+		while(runtime < TIME_QUANTUM &&
+				process[cproc].processing_time < process[cproc].runtime)
 		{
 			runtime++;
 			clock++;
 
-			proccess[current_process].processing_time++;
+			process[cproc].processing_time++;
 		}
 
-		current_process = current_process + 1 % processes;
+		if(process[cproc].processing_time = process[cproc].runtime)
+		{
+			cout << "clock: " << clock;
+			cout << "	process " << cproc << " completed" << endl;
+			processes--;
+		}
+		else
+		{
+			cout << "clock: " << clock;
+			cout << "	process " << cproc << " switched to " << cproc + 1 % processes << endl;
+		}
+
+		cproc = cproc + 1 % processes;
 		runtime = 0;
 	}
 }
