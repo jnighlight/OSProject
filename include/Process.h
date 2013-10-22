26039808de222ce2ebfbeffaf9eb0ade402c8137
Jure Jumalon <jjumalon13@stetson.edu>
2013-10-22 02:21:37 -0400

    Round Robin tested?

--- include/Process.h
+++ include/Process.h
@@ -3,12 +3,14 @@
 
 struct Process
 {
+	int id;
 	int runtime;
 	int deadline;
 	int priority;
 	int arrive_time;
 	int wait_time;
 	int processing_time;
+	bool completed;
 };
 
 #endif
--- src/Round Robin/main.cpp
+++ src/Round Robin/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <Process.h>
 
+#define PROCESSES 10
 #define TIME_QUANTUM 100
 
 using namespace std;
@@ -17,9 +18,11 @@ int main(int argc, const char* argv[])
 	Process process[processes];
 	for(int i = 0; i < processes; i++)
 	{
+		process[i].id              = i;
 		process[i].runtime         = runtimes[i];
 		process[i].arrive_time     = arrival_times[i];
 		process[i].processing_time = 0;
+		process[i].completed       = false;
 	}
 
 	// Begin Round Robin
@@ -36,21 +39,36 @@ int main(int argc, const char* argv[])
 			clock++;
 
 			process[cproc].processing_time++;
+			/*cout << "process: " << cproc;
+			cout << "	processing time: " << process[cproc].processing_time;
+			cout << "	runtime: " << process[cproc].runtime << endl;*/
 		}
 
-		if(process[cproc].processing_time = process[cproc].runtime)
+		if(process[cproc].processing_time == process[cproc].runtime)
 		{
-			cout << "clock: " << clock;
-			cout << "	process " << cproc << " completed" << endl;
+			process[cproc].completed = true;
 			processes--;
-		}
-		else
-		{
 			cout << "clock: " << clock;
-			cout << "	process " << cproc << " switched to " << cproc + 1 % processes << endl;
+			cout << "	process " << process[cproc].id << " out of " << processes << " completed" << endl;
 		}
 
-		cproc = cproc + 1 % processes;
-		runtime = 0;
+		// If there are processes remaining, switch to the next process
+		if(processes > 0)
+		{
+			int prevp = cproc;
+			//cproc = (cproc + 1) % processes;
+
+			// Look for the next uncompleted process
+			do
+			{
+				cproc = (cproc + 1) % PROCESSES;
+			}
+			while(process[cproc].completed);
+
+			cout << "clock: " << clock;
+			cout << "	process " << process[prevp].id << " switched to " << process[cproc].id << endl;
+
+			runtime = 0;
+		}
 	}
 }
