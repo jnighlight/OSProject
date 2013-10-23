b3ef65ecf12b9815dad01f45b022fb6d68e1e60b
jnighlight <jlites@stetson.edu>
2013-10-23 00:25:47 -0400

    made that BEAUTIFUL EDF.cpp work liek a charm. It's amazin. Also accidentally added ad.exe and the EDF.o object. .O well.

--- src/EarliestDeadFirst/EDF.cpp
+++ src/EarliestDeadFirst/EDF.cpp
@@ -13,11 +13,16 @@ bool processSortByArriveTime(Process procA, Process procB)
 	return procA.arrive_time <= procB.arrive_time;
 }
 
+bool processSortByID(Process procA, Process procB)
+{
+	return procA.id <= procB.id;
+}
+
 bool processSortByDeadline(Process procA, Process procB)
 {
-	if(procA.arrive_time < clockerSpaniel)
+	if(procA.arrive_time > clockerSpaniel)
 		{return false;}
-	if(procB.arrive_time < clockerSpaniel)
+	if(procB.arrive_time > clockerSpaniel)
 		{return true;}
 	return procA.deadline <= procB.deadline;
 }
@@ -25,10 +30,10 @@ bool processSortByDeadline(Process procA, Process procB)
 int main()
 {
 	int runtimes[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
-	int ids[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
-	int processingTimes[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
-	int arrivalTimes[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
-	int deadlines[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
+	int ids[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	//int processingTimes[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
+	int arrivalTimes[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; //{ 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
+	int deadlines[10] = { 2, 1, 3, 4, 5, 500, 7, 8, 9, 10};
 	int priorities[10] = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
 	int processesStart = 10;
 	int processesLeft = processesStart;
@@ -42,13 +47,18 @@ int main()
 	{
 		procs[i].id = ids[i];
 		procs[i].runtime = runtimes[i];
-		procs[i].processing_time = processingTimes[i];
+		procs[i].period = 0;
 		procs[i].arrive_time = arrivalTimes[i];
 		procs[i].deadline = deadlines[i];
 		procs[i].priority = priorities[i];
 	}
 	
 	std::sort(procs, procs + processesStart, &processSortByDeadline);
+	cout << "Array sorted:" << endl;
+	for(int j = 0; j < 10; j++)
+	{
+		cout << "process #" << procs[j].id << endl;
+	}
 	
 											//Not necessary anymore - int doneRun = 0;
 	while(processesLeft > 0)  //processesToGo + processesLeft > 0)
@@ -56,17 +66,26 @@ int main()
 		//Increment clock
 		clockerSpaniel++;
 		
+		//sort it so the process with the earliest deadline is first in the "queue"
+		std::sort(procs, procs + processesStart, &processSortByDeadline);
+		cout << "Array sorted:" << endl;
+		for(int j = 0; j < 10; j++)
+		{
+			cout << "process #" << procs[j].id << endl;
+		}
+		
+		
 		//If the processing time is less than the amount of time it needs...
-		if(procs[0].runtime - procs[0].processing_time > 0)
+		if(procs[0].runtime - procs[0].period > 0)
 		{
 			//...and if it has already arrived, increase how long it has been processing
-			if(procs[0].arrive_time >= clockerSpaniel)
-				{procs[0].processing_time++;}
+			if(procs[0].arrive_time <= clockerSpaniel)
+				{procs[0].period++;}
 			//If it hasn't arrived, then none are in the queue, and we're just spinning
 		}
 		else
 		{
-			cout << "finished process # " << procs[[0].id << endl;
+			cout << "finished process # " << procs[0].id << endl;
 			cout << "clock: " << clockerSpaniel << endl;
 			cout << "arrive_time: " << procs[0].arrive_time << endl;
 			cout << "runtime: " << procs[0].runtime << endl << endl;
@@ -76,19 +95,17 @@ int main()
 			
 			processesLeft--;
 			procs[0].deadline = 32766;
-			
-			//sort it so the process with the earliest deadline is first in the "queue"
-			std::sort(procs, procs + processesStart, &processSortByDeadline);
 		}
 		//clockerSpaniel++;
 	}
 	
 	cout << endl;
 	
+	std::sort(procs, procs + processesStart, &processSortByID);
 	for(int i = 0; i < 10; i++)
 	{
 		
-		cout << endl << "Process Number: " << i+1 << endl;
+		cout << endl << "Process Number: " << procs[i].id << endl;
 		cout << procs[i].wait_time  << endl;
 		cout << procs[i].processing_time  << endl;
 	}
